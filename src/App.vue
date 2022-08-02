eb59f5490da85569ca935661383cd53b878d1359
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-02 15:55:44 +0200

    created footer component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <Header />
+  <Footer />
 </template>
 <script>
 import Header from "./components/header/Header.vue";
+import Footer from "./components/footer/Footer.vue";
 export default {
   name: "App",
-  components: { Header },
+  components: { Header, Footer },
 };
 </script>
