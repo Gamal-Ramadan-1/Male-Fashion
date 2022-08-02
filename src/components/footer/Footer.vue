956b5013b3f8df456a7c9ed0e66340a5f6cd298b
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-02 15:59:25 +0200

    footer logo colom

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,5 +1,21 @@
 <template>
-  
+  <footer>
+    <div class="container">
+      <div class="row text-light py-5 gy-4">
+        <div class="col-lg-3">
+          <img src="../../imgs/xfooter-logo.png.pagespeed.ic.itW_wULNzW.webp" />
+          <p class="my-4 text-white-50">
+            The customer is at the heart of our unique business model, which
+            includes design.
+          </p>
+          <img src="../../imgs/xpayment.png.pagespeed.ic.baCZTAO1zx.webp" alt="">
+        </div>
+        <div class="col-lg-3"></div>
+        <div class="col-lg-3"></div>
+        <div class="col-lg-3"></div>
+      </div>
+    </div>
+  </footer>
 </template>
 <script>
 export default {
@@ -8,5 +24,7 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "../../Scss/main.scss";
-
+footer {
+  background-color: $Main-color;
+}
 </style>
