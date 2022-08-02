b1bc844cc9e9322aa276c8a03741577c27732666
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-02 21:46:30 +0200

    fixed padding in  footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <div class="container">
-      <div class="row text-light py-5 gy-4">
+      <div class="row text-light py-5">
         <div class="col-lg-3 col-md-4 col-sm-12">
           <img src="../../imgs/xfooter-logo.png.pagespeed.ic.itW_wULNzW.webp" />
           <p class="my-4 text-white-50">
