59bdf2f9bd29f36536c75f453f5770514987f35d
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-02 16:26:20 +0200

    two sections of shoping

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -8,13 +8,34 @@
             The customer is at the heart of our unique business model, which
             includes design.
           </p>
-          <img
-            src="../../imgs/xpayment.png.pagespeed.ic.baCZTAO1zx.webp"
-            alt=""
-          />
+          <img src="../../imgs/xpayment.png.pagespeed.ic.baCZTAO1zx.webp" />
         </div>
-        <div class="col-lg-3 col-md-4 col-sm-12"></div>
-        <div class="col-lg-3 col-md-4 col-sm-12"></div>
+        <div class="col-lg-3 col-md-4 col-sm-12 d-flex justify-content-center">
+          <div class="shoping">
+            <p class="text-uppercase fw-bold">shoping</p>
+            <span class="text-white-50 text-capitalize mt-4"
+              >clothing store</span
+            >
+            <span class="text-white-50 text-capitalize mt-2"
+              >trending shoes</span
+            >
+            <span class="text-white-50 text-capitalize mt-2">accessories</span>
+            <span class="text-white-50 text-capitalize mt-2">sale</span>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-12">
+          <div class="shoping">
+            <p class="text-uppercase fw-bold">shoping</p>
+            <span class="text-white-50 text-capitalize mt-4">contact us</span>
+            <span class="text-white-50 text-capitalize mt-2"
+              >payment methods</span
+            >
+            <span class="text-white-50 text-capitalize mt-2">delivary</span>
+            <span class="text-white-50 text-capitalize mt-2"
+              >return & exchanges</span
+            >
+          </div>
+        </div>
         <div class="col-lg-3 col-md-4 col-sm-12"></div>
       </div>
     </div>
@@ -29,5 +50,9 @@ export default {
 @import "../../Scss/main.scss";
 footer {
   background-color: $Main-color;
+}
+.shoping span {
+  cursor: pointer;
+  display: block;
 }
 </style>
