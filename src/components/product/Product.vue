f95f71eb5f370138dd6b38b5b23c4cb1708ac3ff
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-09 16:06:30 +0200

    created one card from cards in product section

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -17,7 +17,45 @@
           </h4>
         </div>
       </div>
-      
+      <div class="row box-list">
+        <div class="col-lg-3 col-md-6 col-sm-12 mix box-item">
+          <div class="card-content">
+            <div class="card-img">
+              <h6 class="text-capitalize px-3">new</h6>
+              <div class="content-hover-in-box text-end">
+                <a href="#" class="d-block">
+                  <i class="fa-solid fa-heart h4 p-2 mx-3 mt-4"></i>
+                </a>
+                <a href="#" class="d-block">
+                  <i class="fa-duotone fa-code-compare h4 p-1 mx-3"></i>
+                </a>
+                <a href="#" class="d-block">
+                  <i class="fa-brands fa-searchengin h4 p-2 mx-3"></i>
+                </a>
+              </div>
+            </div>
+            <div class="card-text">
+              <h6>+ add to cart</h6>
+              <p class="mt-3 mb-1">Piqué Biker Jacket</p>
+              <div class="d-block">
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+              </div>
+              <div class="d-flex justify-content-between align-items-center">
+                <h5 class="d-inline-block">$67.24</h5>
+                <div class="colors">
+                  <div class="purple d-inline-block mx-1"></div>
+                  <div class="dark d-inline-block mx-1"></div>
+                  <div class="brouwn d-inline-block mx-1"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -32,4 +70,110 @@ $(document).ready(function () {
 </script>
 <style lang="scss" scoped>
 @import "../../Scss/main.scss";
+a {
+  text-decoration: none;
+  color: $Main-color;
+}
+.card-img {
+  background-image: url("../../imgs/product-1.jpg.webp");
+  background-position: center;
+  clear: both;
+  background-size: cover;
+  height: 300px;
+  position: relative;
+  display: flex;
+  justify-content: end;
+  align-items: flex-start;
+  h6 {
+    top: 30px;
+    position: absolute;
+    background-color: white;
+    left: 0px;
+  }
+}
+
+.content-hover-in-box {
+  right: -30px;
+  visibility: hidden;
+  position: absolute;
+  transition: all 0.3s;
+  opacity: 0;
+  i {
+    background-color: white;
+    font-size: 20px;
+    width: 35px;
+  }
+}
+.box-item {
+  transition: all 0.3s;
+}
+
+.colors {
+  visibility: hidden;
+  opacity: 0;
+  transition: all 0.3s;
+}
+
+.box-item:hover .colors,
+.box-item:hover .card-text h6 {
+  visibility: visible;
+  opacity: 1;
+  transition: all 0.3s;
+}
+
+.box-item:hover .content-hover-in-box {
+  right: 0px;
+  visibility: visible;
+  opacity: 1;
+  transition: all 0.3s;
+  transition-delay: 300ms;
+}
+
+.card-text {
+  position: relative;
+  i {
+    font-size: 14px;
+    color: rgb(183, 177, 177);
+  }
+  h6 {
+    color: $Socend-color;
+    position: absolute;
+    text-transform: capitalize;
+    z-index: 1;
+    opacity: 0;
+    visibility: hidden;
+    top: -20px;
+    cursor: pointer;
+    transition: all 0.3s;
+  }
+}
+
+.box-item:hover .card-text h6 {
+  visibility: visible;
+  cursor: pointer;
+  opacity: 1;
+  transition: all 0.3s;
+  top: 0px;
+}
+
+.box-item:hover .card-text p {
+  transition: all 0.3s;
+
+  opacity: 0;
+}
+.purple,
+.brouwn,
+.dark {
+  background-color: #5e64d1;
+  width: 13px;
+  height: 13px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+.brouwn {
+  background-color: #d5a667;
+}
+.dark {
+  background-color: #404a47;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,28 +3,6 @@
   <Bannar />
   <Categories />
   <Product />
-  <!-- <div class="container">
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="box-menu">
-          <ul>
-            <li class="mixitup-control-active" data-filter="*">all</li>
-            <li data-filter=".photo">photography</li>
-            <li data-filter=".ui">ui / ux</li>
-            <li data-filter=".paint">painting</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="row box-list g-4">
-      <div class="col-lg-4 mix box-item photo ui"></div>
-      <div class="col-lg-4 mix box-item ui"></div>
-      <div class="col-lg-4 mix box-item photo"></div>
-      <div class="col-lg-4 mix box-item paint"></div>
-      <div class="col-lg-4 mix box-item paint ui"></div>
-      <div class="col-lg-4 mix box-item photo paint"></div>
-    </div>
-  </div> -->
 </template>
 <script>
 import Carousel from "@/components/CarouselHome/Carousel.vue";
@@ -35,9 +13,6 @@ export default {
   name: "Home",
   components: { Carousel, Bannar, Categories, Product },
 };
-// $(document).ready(function () {
-//   var mixer = mixitup(".box-list");
-// });
 </script>
 
 <style lang="scss" scoped>
