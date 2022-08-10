2566543025cc814e173fc708188de366afdc4bf4
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-11 00:18:45 +0200

    started in Instagram section

---
+++ src/components/InstagramComp/Instagram.vue
@@ -0,0 +1,12 @@
+<template>
+    
+</template>
+<script>
+export default {
+  name: "Instagram",
+};
+</script>
+<style lang="scss" scoped>
+@import '../../Scss/main.scss';
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,17 @@
   <Bannar />
   <Product />
   <Categories />
+  <Instagram />
 </template>
 <script>
 import Carousel from "@/components/CarouselHome/Carousel.vue";
 import Bannar from "../components/bannar/Bannar.vue";
 import Categories from "@/components/Categories/Categories.vue";
 import Product from "../components/product/Product.vue";
+import Instagram from "../components/InstagramComp/Instagram.vue";
 export default {
   name: "Home",
-  components: { Carousel, Bannar, Categories, Product },
+  components: { Carousel, Bannar, Categories, Product, Instagram },
 };
 </script>
 
