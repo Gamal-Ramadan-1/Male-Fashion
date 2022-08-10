5174c3c185794ba8477a8e36e5ec22a1fe39a28b
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-10 23:26:45 +0200

    this commit becuse i created commit to start instgram component and write commit on this wrong

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,15 @@
   <Bannar />
   <Product />
   <Categories />
-  <Instagram />
 </template>
 <script>
 import Carousel from "@/components/CarouselHome/Carousel.vue";
 import Bannar from "../components/bannar/Bannar.vue";
 import Categories from "@/components/Categories/Categories.vue";
 import Product from "../components/product/Product.vue";
-import Instagram from "@/components/Instagram/Instagram.vue";
 export default {
   name: "Home",
-  components: { Carousel, Bannar, Categories, Product, Instagram },
+  components: { Carousel, Bannar, Categories, Product },
 };
 </script>
 
