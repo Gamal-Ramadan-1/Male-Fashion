79b9dd14a37f9abb587abe964639536e1dc6eaa2
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-14 23:22:48 +0200

    created BreadcrumbOption section

--- src/components/BreadcrumbOptionShopPage/BreadcrumbOption.vue
+++ src/components/BreadcrumbOptionShopPage/BreadcrumbOption.vue
@@ -1,4 +1,22 @@
-<template></template>
+<template>
+  <section class="breadcrumb-option">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h3 class="text-capitalize">shop</h3>
+          <p>
+            <router-link to="/"
+              ><span class="text-dark mx-1">Home </span></router-link
+            >
+            <router-view />
+
+            <span class="text-capitalize"> > shop </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
 <script>
 export default {
   name: "BreadcrumbOption",
@@ -6,4 +24,18 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "@/Scss/main.scss";
+a {
+  text-decoration: none;
+}
+.breadcrumb-option {
+    padding-top: 30px;
+    padding-bottom: 30px;
+  background-color: #f3f2ee;
+  p {
+    span {
+      color: gray;
+      text-decoration: none;
+    }
+  }
+}
 </style>
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,7 +1,10 @@
 <template>
+  <BreadcrumbOption />
 </template>
 <script>
+import BreadcrumbOption from "@/components/BreadcrumbOptionShopPage/BreadcrumbOption.vue";
 export default {
   name: "Shop",
+  components: { BreadcrumbOption },
 };
 </script>
