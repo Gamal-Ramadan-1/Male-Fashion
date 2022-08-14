e408fbf697ca2a255edfef5d9a914ea6624e8b58
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-14 22:39:10 +0200

    add ShopCard component in prodect section

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -36,308 +36,74 @@
       <div class="row box-list gy-4 mb-5">
         <div class="col-lg-3 col-md-6 col-sm-12 mix new-arrivals">
           <div class="box-item">
-            <div class="card-content">
-              <div class="card-img-1">
-                <div class="content-hover-in-box text-end">
-                  <a href="#" class="d-block">
-                    <i class="fa-solid fa-heart h4 p-2 mx-3 mt-4"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-duotone fa-code-compare h4 p-1 mx-3"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-brands fa-searchengin h4 p-2 mx-3"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-text">
-                <h6>+ add to cart</h6>
-                <p class="mt-3 mb-1">Piqué Biker Jacket</p>
-                <div class="d-block">
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                </div>
-                <div class="d-flex justify-content-between align-items-center">
-                  <h5 class="d-inline-block">$67.24</h5>
-                  <div class="colors">
-                    <div class="purple d-inline-block mx-1"></div>
-                    <div class="dark d-inline-block mx-1"></div>
-                    <div class="brouwn d-inline-block mx-1"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ShopCard
+              src="imgs/product-1.jpg.webp"
+              NameCard="pique biker jacket"
+              price="$25.65"
+            />
           </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 mix hot-sales">
           <div class="box-item">
-            <div class="card-content">
-              <div class="card-img-2">
-                <div class="content-hover-in-box text-end">
-                  <a href="#" class="d-block">
-                    <i class="fa-solid fa-heart h4 p-2 mx-3 mt-4"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-duotone fa-code-compare h4 p-1 mx-3"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-brands fa-searchengin h4 p-2 mx-3"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-text">
-                <h6>+ add to cart</h6>
-                <p class="mt-3 mb-1">Piqué Biker Jacket</p>
-                <div class="d-block">
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                </div>
-                <div class="d-flex justify-content-between align-items-center">
-                  <h5 class="d-inline-block">$67.24</h5>
-                  <div class="colors">
-                    <div class="purple d-inline-block mx-1"></div>
-                    <div class="dark d-inline-block mx-1"></div>
-                    <div class="brouwn d-inline-block mx-1"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ShopCard
+              src="imgs/product-2.jpg.webp"
+              NameCard="pique biker jacket"
+              price="$67.25"
+            />
           </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 mix new-arrivals">
           <div class="box-item">
-            <div class="card-content">
-              <div class="card-img-3">
-                <h6 class="text-capitalize px-3 bg-dark text-light">sale</h6>
-                <div class="content-hover-in-box text-end">
-                  <a href="#" class="d-block">
-                    <i class="fa-solid fa-heart h4 p-2 mx-3 mt-4"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-duotone fa-code-compare h4 p-1 mx-3"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-brands fa-searchengin h4 p-2 mx-3"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-text">
-                <h6>+ add to cart</h6>
-                <p class="mt-3 mb-1">Piqué Biker Jacket</p>
-                <div class="d-block">
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                </div>
-                <div class="d-flex justify-content-between align-items-center">
-                  <h5 class="d-inline-block">$67.24</h5>
-                  <div class="colors">
-                    <div class="purple d-inline-block mx-1"></div>
-                    <div class="dark d-inline-block mx-1"></div>
-                    <div class="brouwn d-inline-block mx-1"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ShopCard
+              src="imgs/product-3.jpg.webp"
+              NameCard="multi-pocket chest bag"
+              price="$43.48"
+            />
           </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 mix hot-sales">
           <div class="box-item">
-            <div class="card-content">
-              <div class="card-img-4">
-                <div class="content-hover-in-box text-end">
-                  <a href="#" class="d-block">
-                    <i class="fa-solid fa-heart h4 p-2 mx-3 mt-4"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-duotone fa-code-compare h4 p-1 mx-3"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-brands fa-searchengin h4 p-2 mx-3"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-text">
-                <h6>+ add to cart</h6>
-                <p class="mt-3 mb-1">Piqué Biker Jacket</p>
-                <div class="d-block">
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                </div>
-                <div class="d-flex justify-content-between align-items-center">
-                  <h5 class="d-inline-block">$67.24</h5>
-                  <div class="colors">
-                    <div class="purple d-inline-block mx-1"></div>
-                    <div class="dark d-inline-block mx-1"></div>
-                    <div class="brouwn d-inline-block mx-1"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ShopCard
+              src="imgs/product-4.jpg.webp"
+              NameCard="Diagonal Textured Cap"
+              price="$60.9"
+            />
           </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 mix new-arrivals">
           <div class="box-item">
-            <div class="card-content">
-              <div class="card-img-5">
-                <div class="content-hover-in-box text-end">
-                  <a href="#" class="d-block">
-                    <i class="fa-solid fa-heart h4 p-2 mx-3 mt-4"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-duotone fa-code-compare h4 p-1 mx-3"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-brands fa-searchengin h4 p-2 mx-3"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-text">
-                <h6>+ add to cart</h6>
-                <p class="mt-3 mb-1">Piqué Biker Jacket</p>
-                <div class="d-block">
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                </div>
-                <div class="d-flex justify-content-between align-items-center">
-                  <h5 class="d-inline-block">$67.24</h5>
-                  <div class="colors">
-                    <div class="purple d-inline-block mx-1"></div>
-                    <div class="dark d-inline-block mx-1"></div>
-                    <div class="brouwn d-inline-block mx-1"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ShopCard
+              src="imgs/product-5.jpg.webp"
+              NameCard="Lether Backpack"
+              price="$31.37"
+            />
           </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 mix hot-sales">
           <div class="box-item">
-            <div class="card-content">
-              <div class="card-img-6">
-                <h6 class="text-capitalize px-3 bg-dark text-light">sale</h6>
-                <div class="content-hover-in-box text-end">
-                  <a href="#" class="d-block">
-                    <i class="fa-solid fa-heart h4 p-2 mx-3 mt-4"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-duotone fa-code-compare h4 p-1 mx-3"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-brands fa-searchengin h4 p-2 mx-3"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-text">
-                <h6>+ add to cart</h6>
-                <p class="mt-3 mb-1">Piqué Biker Jacket</p>
-                <div class="d-block">
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                </div>
-                <div class="d-flex justify-content-between align-items-center">
-                  <h5 class="d-inline-block">$67.24</h5>
-                  <div class="colors">
-                    <div class="purple d-inline-block mx-1"></div>
-                    <div class="dark d-inline-block mx-1"></div>
-                    <div class="brouwn d-inline-block mx-1"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ShopCard
+              src="imgs/product-6.jpg.webp"
+              NameCard="Ankle Boots"
+              price="$98.66"
+            />
           </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 mix new-arrivals">
           <div class="box-item">
-            <div class="card-content">
-              <div class="card-img-7">
-                <div class="content-hover-in-box text-end">
-                  <a href="#" class="d-block">
-                    <i class="fa-solid fa-heart h4 p-2 mx-3 mt-4"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-duotone fa-code-compare h4 p-1 mx-3"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-brands fa-searchengin h4 p-2 mx-3"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-text">
-                <h6>+ add to cart</h6>
-                <p class="mt-3 mb-1">Piqué Biker Jacket</p>
-                <div class="d-block">
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                </div>
-                <div class="d-flex justify-content-between align-items-center">
-                  <h5 class="d-inline-block">$67.24</h5>
-                  <div class="colors">
-                    <div class="purple d-inline-block mx-1"></div>
-                    <div class="dark d-inline-block mx-1"></div>
-                    <div class="brouwn d-inline-block mx-1"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ShopCard
+              src="imgs/product-7.jpg.webp"
+              NameCard="T-shirt Contrast Pocket"
+              price="$49.66"
+            />
           </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 mix hot-sales">
           <div class="box-item">
-            <div class="card-content">
-              <div class="card-img-8">
-                <div class="content-hover-in-box text-end">
-                  <a href="#" class="d-block">
-                    <i class="fa-solid fa-heart h4 p-2 mx-3 mt-4"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-duotone fa-code-compare h4 p-1 mx-3"></i>
-                  </a>
-                  <a href="#" class="d-block">
-                    <i class="fa-brands fa-searchengin h4 p-2 mx-3"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-text">
-                <h6>+ add to cart</h6>
-                <p class="mt-3 mb-1">Piqué Biker Jacket</p>
-                <div class="d-block">
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                </div>
-                <div class="d-flex justify-content-between align-items-center">
-                  <h5 class="d-inline-block">$67.24</h5>
-                  <div class="colors">
-                    <div class="purple d-inline-block mx-1"></div>
-                    <div class="dark d-inline-block mx-1"></div>
-                    <div class="brouwn d-inline-block mx-1"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ShopCard
+              src="imgs/product-8.jpg.webp"
+              NameCard="Basic Flowing Scarf"
+              price="$26.28"
+            />
           </div>
         </div>
       </div>
@@ -392,144 +158,9 @@ a {
     cursor: pointer;
   }
 }
-.card-img-1,
-.card-img-2,
-.card-img-3,
-.card-img-4,
-.card-img-5,
-.card-img-6,
-.card-img-7,
-.card-img-8 {
-  background-position: center;
-  clear: both;
-  background-size: cover;
-  height: 300px;
-  position: relative;
-  display: flex;
-  justify-content: end;
-  align-items: flex-start;
-  h6 {
-    top: 30px;
-    position: absolute;
-    background-color: white;
-    left: 0px;
-  }
-}
-
-.card-img-1 {
-  background-image: url("../../imgs/product-1.jpg.webp");
-}
-
-.card-img-2 {
-  background-image: url("../../imgs/product-2.jpg.webp");
-}
-
-.card-img-3 {
-  background-image: url("../../imgs/product-3.jpg.webp");
-}
-
-.card-img-4 {
-  background-image: url("../../imgs/product-4.jpg.webp");
-}
-
-.card-img-5 {
-  background-image: url("../../imgs/product-5.jpg.webp");
-}
-
-.card-img-6 {
-  background-image: url("../../imgs/product-6.jpg.webp");
-}
-
-.card-img-7 {
-  background-image: url("../../imgs/product-7.jpg.webp");
-}
-
-.card-img-8 {
-  background-image: url("../../imgs/product-8.jpg.webp");
-}
-
-.content-hover-in-box {
-  right: -10px;
-  visibility: hidden;
-  position: absolute;
-  transition: all 0.3s;
-  opacity: 0;
-  i {
-    background-color: white;
-    font-size: 20px;
-    width: 35px;
-  }
-}
 .box-item {
   transition: all 0.3s;
 }
 
-.colors {
-  visibility: hidden;
-  opacity: 0;
-  transition: all 0.3s;
-}
 
-.box-item:hover .colors,
-.box-item:hover .card-text h6 {
-  visibility: visible;
-  opacity: 1;
-  transition: all 0.3s;
-}
-
-.box-item:hover .content-hover-in-box {
-  right: 0px;
-  visibility: visible;
-  opacity: 1;
-  transition: all 0.3s;
-  transition-delay: 300ms;
-}
-
-.card-text {
-  position: relative;
-  i {
-    font-size: 14px;
-    color: rgb(183, 177, 177);
-  }
-  h6 {
-    color: $Socend-color;
-    position: absolute;
-    text-transform: capitalize;
-    z-index: 1;
-    opacity: 0;
-    visibility: hidden;
-    top: -20px;
-    cursor: pointer;
-    transition: all 0.3s;
-  }
-}
-
-.box-item:hover .card-text h6 {
-  visibility: visible;
-  cursor: pointer;
-  opacity: 1;
-  transition: all 0.3s;
-  top: 0px;
-}
-
-.box-item:hover .card-text p {
-  transition: all 0.3s;
-
-  opacity: 0;
-}
-.purple,
-.brouwn,
-.dark {
-  background-color: #5e64d1;
-  width: 13px;
-  height: 13px;
-  border-radius: 50%;
-  cursor: pointer;
-}
-.brouwn {
-  background-color: #d5a667;
-}
-.dark {
-  background-color: #404a47;
-}
 </style>
