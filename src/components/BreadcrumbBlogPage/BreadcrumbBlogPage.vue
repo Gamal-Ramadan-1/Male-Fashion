d5cf6663bfd1cd6bbcd19eb5e45d4c9fc958c134
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-17 18:12:39 +0200

    created BreadCrumbBlogPage component

---
+++ src/components/BreadcrumbBlogPage/BreadcrumbBlogPage.vue
@@ -0,0 +1,9 @@
+<template></template>
+<script>
+export default {
+  name: "BreadcrumbBlogPage",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Blog.vue
@@ -0,0 +1,12 @@
+<template>
+  <BreadcrumbBlogPage />
+</template>
+<script>
+import BreadcrumbBlogPage from "@/components/BreadcrumbBlogPage/BreadcrumbBlogPage.vue";
+export default {
+  name: "Blog",
+  components: { BreadcrumbBlogPage },
+};
+</script>
+<style lang="scss" scoped>
+</style>
