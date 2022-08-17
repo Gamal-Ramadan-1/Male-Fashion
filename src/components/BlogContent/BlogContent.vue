e970118637d55cba0264ccb5b9f2337a36a140e6
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-17 18:39:40 +0200

    start in BlogContent component

---
+++ src/components/BlogContent/BlogContent.vue
@@ -0,0 +1,10 @@
+<template></template>
+<script>
+export default {
+  name: "BlogContent",
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,11 +1,13 @@
 <template>
   <BreadcrumbBlogPage />
+  <BlogContent />
 </template>
 <script>
 import BreadcrumbBlogPage from "@/components/BreadcrumbBlogPage/BreadcrumbBlogPage.vue";
+import BlogContent from "@/components/BlogContent/BlogContent.vue";
 export default {
   name: "Blog",
-  components: { BreadcrumbBlogPage },
+  components: { BreadcrumbBlogPage, BlogContent },
 };
 </script>
 <style lang="scss" scoped>
