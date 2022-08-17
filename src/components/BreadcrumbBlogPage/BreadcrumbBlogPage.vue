26159d2e440e2c41ca9732fc163b34cba8a20d7a
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-17 18:31:18 +0200

    finshed BreadcrumbBlogPage

--- src/components/BreadcrumbBlogPage/BreadcrumbBlogPage.vue
+++ src/components/BreadcrumbBlogPage/BreadcrumbBlogPage.vue
@@ -1,4 +1,14 @@
-<template></template>
+<template>
+  <section class="Breadcrumb-Blog">
+    <div class="container h-100">
+      <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-12 text-center text-light text-capitalize">
+          <h1 class="display-2 our-blog">our blog</h1>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
 <script>
 export default {
   name: "BreadcrumbBlogPage",
@@ -6,4 +16,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/Scss/main.scss";
+.Breadcrumb-Blog {
+  background-image: url("@/imgs/breadcrumb-bg.jpg.webp");
+  height: 300px;
+  background-size: cover;
+  clear: both;
+  background-position: center;
+}
+
+.our-blog {
+  font-weight: 500;
+}
 </style>
