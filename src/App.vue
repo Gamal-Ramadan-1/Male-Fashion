5b1715f947adba41d9bfda117179afa9af210113
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-27 17:20:57 +0200

    add Up to Top Button

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
+  <Up />
   <Header />
   <Footer />
 </template>
 <script>
 import Header from "./components/header/Header.vue";
 import Footer from "./components/footer/Footer.vue";
+import Up from "./components/UpToTop/Up.vue";
 export default {
   name: "App",
-  components: { Header, Footer },
+  components: { Header, Footer, Up },
 };
 </script>
---
+++ src/components/UpToTop/Up.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="up" v-show="scY > 300" @click="toTop()">
+    <i class="fa-solid fa-arrow-up"></i>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      scY: 0,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll: function () {
+      this.scY = window.scrollY;
+    },
+    toTop: () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "@/Scss/main.scss";
+#up {
+  position: fixed;
+  right: 10px;
+  padding: 10px 17px;
+  border-radius: 10px;
+  cursor: pointer;
+  color: white;
+  bottom: 10px;
+  z-index: 1000;
+  background-image: linear-gradient($Main-color, $Socend-color);
+}
+</style>
